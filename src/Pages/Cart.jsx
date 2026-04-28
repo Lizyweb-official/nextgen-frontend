c18d4ad1fc5319df49af41fc4e4a3e4750036d59
riyazlizy <riyas@lizyweb.in>
2026-04-28 12:55:06 +0530

    cart page created

---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,15 @@
+import '../css/style-1.css';
+import '../css/style-2.css';
+import '../css/style-3.css';
+import '../css/style-4.css';
+import '../css/style.css';
+
+function Cart(){
+    return(
+        <>
+
+        </>
+    );
+}
+
+export default Cart;
