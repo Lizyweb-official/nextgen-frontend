b976986c16606a0b28b5b25db4f409a50ec0cd86
RakeshLizyweb <rakesh@lizyweb.in>
2026-05-15 14:50:06 +0530

    last updation

--- src/panel/dp/OrdersDp.jsx
+++ src/panel/dp/OrdersDp.jsx
@@ -1,8 +1,8 @@
-import '../css/style-1.css';
-import '../css/style-2.css';
-import '../css/style-3.css';
-import '../css/style-4.css';
-import '../css/style.css';
+import '../../css/style-1.css';
+import '../../css/style-2.css';
+import '../../css/style-3.css';
+import '../../css/style-4.css';
+import '../../css/style.css';
 
 
 function OrderDp(){
